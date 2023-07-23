d9a0397add862150983f2286b16b546a5f817409
岩本　好平 <koheiiwamoto@iwamotoyoshimitairanoMacBook-Pro.local>
2023-07-23 23:13:38 +0900

    crud

---
+++ python_project/src/module/db/dataManipulation.py
@@ -0,0 +1,17 @@
+
+
+class DataManipulation:
+    def __init__(self):
+        self.conn = conn
+
+    def read(self):
+        pass
+    
+    def insert(self):
+        pass
+
+    def upload(self):
+        pass
+
+    def delete(self):
+        pass
