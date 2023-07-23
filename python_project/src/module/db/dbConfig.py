1bacaff5343ba9fe2bb563e4766c270486a36d21
岩本　好平 <koheiiwamoto@iwamotoyoshimitairanoMacBook-Pro.local>
2023-07-23 23:05:43 +0900

    修正

---
+++ python_project/src/module/db/dbConfig.py
@@ -0,0 +1,9 @@
+# mysql
+MYSQL_DEF = {
+    'python' : {
+        'user': 'docker',
+        'password': 'docker',
+        'host': '192.168.2.2',
+        'database': 'python',
+    },
+}
--- python_project/src/module/db/mysqlConnector.py
+++ python_project/src/module/db/mysqlConnector.py
@@ -1,24 +1,16 @@
+import dbConfig as conf
 import mysql.connector as mySQLConn
 from dbConnector import DbConnector
-# from mysql.connector import errorCode
-
-db_config = {
-    'python' : {
-        'user': 'docker',
-        'password': 'docker',
-        'host': '192.168.2.2',
-        'database': 'python',
-    },
-}
 
 class MysqlConnector(DbConnector):
     def __init__(self, db):
-        self.conf = db_config[db]
+        self.conf = conf.MYSQL_DEF[db]
 
     def connect(self):
         try:
             conn = mySQLConn.connect(**self.conf)
             print(conn.is_connected())
             return conn
-        except Exception:
+        except Exception as e:
+            print(e)
             raise
