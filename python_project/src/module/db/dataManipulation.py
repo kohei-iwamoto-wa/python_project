55956de54d45636702dff443e775e9bab6fc3934
岩本　好平 <koheiiwamoto@iwamotoyoshimitairanoMacBook-Pro.local>
2023-07-27 06:18:37 +0900

    微修正しました。

--- python_project/src/module/db/dataManipulation.py
+++ python_project/src/module/db/dataManipulation.py
@@ -4,10 +4,11 @@ class DataManipulation:
     def __init__(self, conn):
         self.conn = conn
 
-    def iter(self, sql):
+    def read(self, sql):
         try:
             cur = self.conn.cursor(buffered=True)
             cur.execute(sql)
+            cur.fetchOne
             return cur
         except Exception as e:
             print(type(e))
@@ -15,9 +16,10 @@ class DataManipulation:
         finally:
             cur.close()
 
-    def select_all():
+    def select_all(self, sql):
         try:
             cur = self.conn.cursor()
+            cur.execute(sql)
             rows = cur.fetchall()
             return rows
         except Exception as e:
---
+++ python_project/src/module/util/sanitizer.py
@@ -0,0 +1,4 @@
+
+class Sanitaizer:
+    def __init__(self):
+        pass
